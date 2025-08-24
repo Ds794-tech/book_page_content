89512dcc03c6e6739242e6133f85b6bf58fb4667
Ds794-tech <dixitsoni643@gmail.com>
2025-08-25 00:12:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Control how entry chunks (JS) are named
+        entryFileNames: "assets/js/[name]-[hash].js",
+        // Control how code-split chunks are named
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        // Control how CSS & other assets are named
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith(".css")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+          return "assets/[ext]/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 })
